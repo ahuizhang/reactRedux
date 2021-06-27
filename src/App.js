e2f61413395106d1edd1dc3baaddf7483082b393
agui <1720236230@qq.com>
2021-06-27 08:56:31 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {add,inc} from './store/createActions/ageAction'
 @connect(
